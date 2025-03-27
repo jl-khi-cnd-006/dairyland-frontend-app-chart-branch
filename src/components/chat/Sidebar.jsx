0220470f8eaabecd5ce881eafdbfe4330c208584
L-javeria <bintemurad2000@gmail.com>
2025-03-27 14:31:29 +0500

    landing page with switching logic is attached

--- src/components/chat/Sidebar.jsx
+++ src/components/chat/Sidebar.jsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import React, { useEffect, useRef, useState } from "react";
 import { FaBars } from "react-icons/fa6";
 import { IoClose } from "react-icons/io5";
+import { useRouter } from "next/navigation";
 
 // const freqData = [
 //   // {
@@ -77,6 +78,7 @@ const freqData = [
 function SideBar({ handleFrqClick }) {
   const [sidebarOpen, setSidebarOpen] = useState(false);
   const sidebarRef = useRef(null);
+  const router = useRouter()
 
   useEffect(() => {
     const handleClickOutside = (event) => {
